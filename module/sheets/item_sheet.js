2e11e552c365739a0a472d7125de504e53a9b85e
lokdal <leblvin@gmail.com>
2021-01-30 12:25:15 -0500

    setup css auto-compile with less

--- module/sheets/item_sheet.js
+++ module/sheets/item_sheet.js
@@ -1,4 +1,13 @@
 export default class item_sheet extends ItemSheet {
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 530,
+            height: 340,
+            classes: ["intersection", "sheet", "item"]
+        });
+    };
+
     get template() {
         return `systems/intersection/templates/sheets/${this.item.data.type}_sheet.html`
     };
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 Follow the instructions here: https://foundryvtt.com/article/system-development/
+Video guide on system building: https://www.youtube.com/playlist?list=PLFV9z59nkHDccUbRXVt623UdloPTclIrz
